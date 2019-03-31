ec5a46c87434ae123728755be7e2c4983322b2bb
devarshi16 <devershigpt6@gmail.com>
2019-03-31 18:30:41 +0530

    Initial Commit

---
+++ welcome.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo 'echo "Welcome Devarshi "' >> ~/.bashrc
+echo 'echo "\                     "' >> ~/.bashrc
+echo 'echo " \`_               _/|"' >> ~/.bashrc
+echo 'echo "  \ \_          __/ /"' >> ~/.bashrc
+echo 'echo "   \  \________/   /"' >> ~/.bashrc
+echo 'echo "    |              |"' >> ~/.bashrc
+echo 'echo "    /              |"' >> ~/.bashrc
+echo 'echo "   |   0       0   |"' >> ~/.bashrc
+echo 'echo "   |       _       |"' >> ~/.bashrc
+echo 'echo "   |()    __    () |"' >> ~/.bashrc
+echo 'echo "    \    (__)      |"' >> ~/.bashrc
+echo 'file="/home/$USER/fortunes.txt"' >> ~/.bashrc
+echo 'if [ -f "$file" ]' >> ~/.bashrc
+echo 'then' >> ~/.bashrc
+echo '    shuf -n 1 $file' >> ~/.bashrc
+echo 'fi' >> ~/.bashrc
+cp ./fortunes.txt /home/$USER/fortunes.txt
