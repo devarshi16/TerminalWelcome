77c83a3710a95159c5714188ebdff96795ad7ef9
devarshi16 <devershigpt6@gmail.com>
2020-01-17 22:59:22 +0530

    Config parser stuff

--- echo_to_screen.py
+++ echo_to_screen.py
@@ -4,15 +4,16 @@ from one_liners import one_liners
 from poke_config import config
 from pokemons import pokemons
 import random
+import configparser
 
-def display_one_liner():
+def display_one_liner(config):
     if config["one-liner"]:
         print(random.choice(one_liners))
 
-def display_ascii_art():
+def display_ascii_art(config):
     print(pokemons[config["pokemon"]])
 
-def display_message():
+def display_message(config):
     if config["message"] == None:
         return
     else:
